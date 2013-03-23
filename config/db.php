cc33506c96efa50f1801b5334a1653b0f96878cf
dw33zy12 <harridam@hotmail.com>
2013-03-23 15:36:15 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@
  * This is the place where your database constants are saved
  */
 
-define("DB_HOST", "127.0.0.1");
-define("DB_NAME", "login");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_HOST", "us-cdbr-azure-east-b.cloudapp.net");
+define("DB_NAME", "therealALQGdGZ3k");
+define("DB_USER", "b34f8408374e60");
+define("DB_PASS", "09198f03");
